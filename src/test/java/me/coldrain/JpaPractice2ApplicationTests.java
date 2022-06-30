9b358b3ca7102eeb80be14c3f24174f08dd944a5
coldrain-f <intmain__@naver.com>
2022-07-01 01:41:06 +0900

    Initial commit

---
+++ src/test/java/me/coldrain/JpaPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.coldrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPractice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
